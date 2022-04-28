6a90882a7fdef15cd6e399e76ddc47507675b91c
McWojownik <49338133+McWojownik@users.noreply.github.com>
2022-04-29 01:48:05 +0200

    end quiz finished + add quiz

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from kivy.app import App
 from Screens.StartScreen import StartScreen
 from Screens.CategoryScreen import CategoryScreen
 from Screens.QuizScreen import QuizScreen
-from QuizPy.Screens.QuestionsTypes.ChooseOneScreen import ChooseOneScreen
-from QuizPy.Screens.QuestionsTypes.CorrectOrderScreen import CorrectOrderScreen
+from Screens.QuestionsTypes.ChooseOneScreen import ChooseOneScreen
+from Screens.QuestionsTypes.CorrectOrderScreen import CorrectOrderScreen
 from Screens.LoginScreen import LoginScreen
 from Screens.RegisterScreen import RegisterScreen
 from Screens.CreateScreen import CreateScreen
@@ -23,9 +23,9 @@ class QuizPyApp(App):
     def build(self):
         sm.add_widget(LoginScreen(sm, connection, name="login"))
         sm.add_widget(RegisterScreen(sm, connection, name="register"))
-        sm.add_widget(StartScreen(sm, connection, name="start")
-        sm.add_widget(CategoryScreen(name="category"))
-        sm.add_widget(QuizScreen(name="quiz"))
+        sm.add_widget(StartScreen(sm, connection, name="start"))
+        sm.add_widget(CategoryScreen(sm, connection, name="category"))
+        sm.add_widget(QuizScreen(sm, name="quiz"))
         sm.add_widget(ChooseOneScreen(sm,name="chooseOne"))
         sm.add_widget(CorrectOrderScreen(sm,name="correctOrder"))
         sm.add_widget(CreateScreen(sm, connection, name="create"))
